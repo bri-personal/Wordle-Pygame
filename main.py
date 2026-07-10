6d4a3e0830f7f5946a1b62b86230b3296432de0c
bri-personal <runnerb2003@gmail.com>
2026-07-09 22:01:37 -0700

    move letters on buttons

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ class Game:
             img = pygame.Surface((LETTER_BUTTON_SIZE, LETTER_BUTTON_SIZE))
             img.fill(WHITE)
             img_rect = img.get_rect()
-            draw_text(img, let, LETTER_BUTTON_SIZE * 2 // 3, BLACK, img_rect.width // 2, BORDER, 'midtop')
+            draw_text(img, let, LETTER_BUTTON_SIZE * 2 // 3, BLACK, img_rect.width // 2, img_rect.height // 2, 'center')
             self.letter_buttons[let] = LetterButton(self, x, y, img)
             x += LETTER_BUTTON_SIZE + BORDER
 
@@ -80,7 +80,7 @@ class Game:
             img = pygame.Surface((LETTER_BUTTON_SIZE, LETTER_BUTTON_SIZE))
             img.fill(WHITE)
             img_rect = img.get_rect()
-            draw_text(img, let, LETTER_BUTTON_SIZE * 2 // 3, BLACK, img_rect.width // 2, BORDER, 'midtop')
+            draw_text(img, let, LETTER_BUTTON_SIZE * 2 // 3, BLACK, img_rect.width // 2, img_rect.height // 2, 'center')
             self.letter_buttons[let] = LetterButton(self, x, y, img)
             x += LETTER_BUTTON_SIZE + BORDER
 
